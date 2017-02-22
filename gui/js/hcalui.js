5b044a45fa4edf405d2ba5bf4b802cbef075e113
John Hakala <john_hakala@brown.edu>
2017-02-21 18:08:53 -0600

    pare down the preview of the masked resources to show only FMs

--- gui/js/hcalui.js
+++ gui/js/hcalui.js
@@ -200,14 +200,19 @@ function fillMask() {
       finalMasks.push(userXMLmaskedApps[i]);
     }
     $('#MASKED_RESOURCES').val(JSON.stringify(finalMasks));
-    $('#maskTest').html($('#MASKED_RESOURCES').val());
+    var masksToShow = "";
+    var availableResources = getAvailableResources();
+    $.each(finalMasks, function(index, maskedResource) {
+      if ( $.inArray(maskedResource, availableResources) > -1){
+        masksToShow += maskedResource + ", ";
+      }
+    });
+    masksToShow = masksToShow.slice(0, -2);
+    $('#maskTest').text(masksToShow);
 }
 
 function getAvailableResources() {
-    var param = $('#AVAILABLE_RESOURCES').html().replace("[", "").replace("]","");
-    param =  param.replace(/['"]+/g, '');
-    var array = param.split(',');
-    return array;
+    return JSON.parse($('#AVAILABLE_RESOURCES').val());
 }
 
 function makecheckboxes() {
