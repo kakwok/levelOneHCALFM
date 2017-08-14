b8b2db07ebac5afec0428a2e163aa49b323d77f8
kakwok <ka_hei_martin_kwok@brown.edu>
2017-08-14 11:58:20 +0200

    Update build script to make releases

--- installation/buildHCALFM.sh
+++ installation/buildHCALFM.sh
@@ -2,17 +2,39 @@
 # Script to keep the HCALFM builds organized
 # Production HCALFMs must be built from an unmodified git commit, and are indexed by their commit hash.
 # Test HCALFMs are organized by date, appended with a version number.
-# Usage: ./buildHCALFM.sh test
-# The option can be either "test" or "release".
-# John Hakala 4/14/2016
+# Release convention: yy.xx.zz   where yy = year, xx= major version, zz=minor version
+# Usage 1) test build
+#       ./buildHCALFM.sh test
+# Usage 2) Build major release
+#       ./buildHCALFM.sh release major
+# Usage 3) Build minor release: 
+#       ./buildHCALFM.sh release minor
+# OR  
+#       ./buildHCALFM.sh release
+# Created: John Hakala 4/14/2016
+# Modified: Martin Kwok 8/14/2017
 
 if [ "$1" = "release" ]; then
   git diff-index --quiet HEAD
-  if [ "$?" = "0" ]; then
-    GITREV=`git rev-parse HEAD | head -c 7`
+  if [ "$?" == "0" ]; then
+    release=`git tag -l | tail -1`
+    versionArr=(${release//./ })
+    if [ "$2" = "annual" ]; then
+      GITREV="$((versionArr[0]+1)).0.0"
+      GITREV_fname="$((versionArr[0]+1))_0_0"
+    elif [ "$2" = "major" ]; then
+      GITREV="${versionArr[0]}.$((versionArr[1]+1)).0"
+      GITREV_fname="${versionArr[0]}_$((versionArr[1]+1))_0"
+    else
+      #Default is minor increment
+      GITREV="${versionArr[0]}.${versionArr[1]}.$((versionArr[2]+1))"
+      GITREV_fname="${versionArr[0]}_${versionArr[1]}_$((versionArr[2]+1))"
+    fi
+    echo "Building HCALFM release: $GITREV"
+    git tag $GITREV 
     sed -i '$ d' ../gui/jsp/footer.jspf
     echo "<div id='hcalfmVersion'>HCALFM version: ${GITREV}</div>" >> ../gui/jsp/footer.jspf
-    ant -DgitRev="${GITREV}"
+    ant -DgitRev="${GITREV_fname}"
   else
     echo "No changes since the last commit are permitted when building a release FM. Please commit your changes or stash them."
     exit 1
